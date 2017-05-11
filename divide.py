9947c674bd5e5990f9db64437ed3d760bc0aecc3
libojia-aug <1012139091@qq.com>
2017-05-11 21:26:43 +0800

    add divide

---
+++ divide.py
@@ -0,0 +1,16 @@
+def divide(num):
+    file_object = open('data/'+'DANGLING_PAGE_PR','r')
+    try:
+    	file_object_writed = open('output/'+str(num)+'.txt', 'a')
+        all_the_text = file_object.readlines()
+        for x in xrange(0, len(all_the_text)):
+            print all_the_text[x]
+            line = long(all_the_text[x]) / num
+            file_object_writed.write(str(line)+'\n')
+        
+        file_object_writed.close()
+        file_object.close()
+    finally:
+        file_object_writed.close()
+
+divide(100000000.0)
